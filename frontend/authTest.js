f14a4e357b9f897ad8427bfa2fb1ccec4f872bb7
CaptainUES69 <69493833+CaptainUES69@users.noreply.github.com>
2025-04-15 09:31:58 +0500

    update

--- frontend/authTest.js
+++
@@ -1,9 +0,0 @@
-// задали константы
-const submitBtn = document.querySelector(".submit-btn");
-const auth = document.querySelector(".auth");
-const body = document.querySelector('.body');
-
-// функция не работает. идея: отключение класса окна, что поверх body
-submitBtn.addEventListener('click', () =>{
-    
-})
